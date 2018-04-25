8c14589a47d16303ac937348e88c54f6f5ddfce4
sengming <sengming@omnimatics.io>
2018-04-25 17:28:23 +0800

    Adding SIM scripts

---
+++ sim808_on.py
@@ -0,0 +1,13 @@
+
+import RPi.GPIO as GPIO
+from datetime import datetime
+import time
+
+GPIO.setwarnings(False)
+SIM808_PWR_CTRL = 18 #BCM PIN 17(BOARD PIN 11)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SIM808_PWR_CTRL,GPIO.OUT)
+GPIO.output(SIM808_PWR_CTRL,GPIO.HIGH)
+time.sleep(1)
+GPIO.output(SIM808_PWR_CTRL,GPIO.LOW)
+time.sleep(1)
